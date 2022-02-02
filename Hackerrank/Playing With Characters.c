653f2cc2fba703daeb529e4dff33acc4a128c39c
oii-nasif <nasif.ishtiaque.islam@gmail.com>
2022-02-02 23:51:19 +0600

    playing with characters

---
+++ Hackerrank/Playing With Characters.c
@@ -0,0 +1,25 @@
+//  https://www.hackerrank.com/challenges/playing-with-characters/problem
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    char ch; 
+    char s[100];
+    char sen[100]; 
+    
+    scanf("%c",&ch);
+    scanf("%s",s);
+    scanf("\n"); 
+    scanf("%[^\n]%*s",sen);
+    
+    printf("%c\n",ch); 
+    printf("%s\n",s); 
+    printf("%s",sen); 
+    
+    return 0;
+}
