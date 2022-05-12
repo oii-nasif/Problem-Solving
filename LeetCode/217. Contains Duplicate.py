b078830214c13b254f1902f4a48976ff868988ff
oii-nasif <nasif.ishtiaque.islam@gmail.com>
2022-05-12 23:03:39 +0600

    217

---
+++ LeetCode/217. Contains Duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        distinct_list = list(set(nums))
+        if len(distinct_list) == len(nums):
+            return False
+        else:
+            return True
+        
